4672b510d5b12ef4939aada0fcd5e27ddb21c492
anigwe <7919212+anigwe@users.noreply.github.com>
2019-10-26 14:12:51 -0700

    Add files via upload

--- binder/install.R
+++ binder/install.R
@@ -1,3 +1,3 @@
-pkgs = c("phyloseq","vegan","dplyr","ggplot2","TSA","viridis","ggpubr")
+pkgs = c("knitr","phyloseq","vegan","dplyr","ggplot2","TSA","viridis","ggpubr")
 ncores = parallel::detectCores()
 install.packages(pkgs, Ncpus = ncores)
